8909652e9de4e5260eb99191e2e32d0fc6893ff1
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-08-08 09:13:39 +0500

    Created IHospitalRepository

---
+++ src/OnMed.DataAccess/Interfaces/Hospitals/IHospitalRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnMed.DataAccess.Common.Interfaces;
+using OnMed.DataAccess.ViewModels;
+using OnMed.Domain.Entities.Hospitals;
+
+namespace OnMed.DataAccess.Interfaces.Hospitals;
+
+public interface IHospitalRepository : IRepository<Hospital,HospitalViewModel>,
+    IGetAll<HospitalViewModel>,ISearchable<HospitalViewModel>
+{
+
+}
