82d72c45c2e5d1e415418a945aabaaada2011892
ahadulla <ahadulla1503@gmail.com>
2023-08-08 08:53:41 +0500

    IAdministratorRepository created

---
+++ src/OnMed.DataAccess/Interfaces/Administrators/IAdministratorRepository.cs
@@ -0,0 +1,10 @@
+﻿using OnMed.DataAccess.Common.Interfaces;
+using OnMed.DataAccess.ViewModels.Administrators;
+using OnMed.Domain.Entities.Administrators;
+
+namespace OnMed.DataAccess.Interfaces.Administrators;
+
+public interface IAdministratorRepository : IRepository<Administrator, AdministratorViewModel>,
+    ISearchable<AdministratorViewModel>, IGetAll<AdministratorViewModel>
+{
+}
