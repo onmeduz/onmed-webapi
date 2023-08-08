05affa31343f7941b589fae6db9ee620a6ec69b2
ahadulla <ahadulla1503@gmail.com>
2023-08-08 08:57:00 +0500

    Created ICategoryRepository

---
+++ src/OnMed.DataAccess/Interfaces/Categories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnMed.DataAccess.Common.Interfaces;
+using OnMed.DataAccess.ViewModels.Categories;
+using OnMed.Domain.Entities.Categories;
+
+namespace OnMed.DataAccess.Interfaces.Categories;
+
+public interface ICategoryRepository : IRepository<Category, CategoryViewModel>, IGetAll<CategoryViewModel>
+{
+}
