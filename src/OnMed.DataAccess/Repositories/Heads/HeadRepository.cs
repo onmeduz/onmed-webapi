256bfad01c0c0dfa1ba7128510e48208eee12bf0
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-08-09 08:49:56 +0500

    2 - commit

--- src/OnMed.DataAccess/Repositories/Heads/HeadRepository.cs
+++ src/OnMed.DataAccess/Repositories/Heads/HeadRepository.cs
@@ -1,10 +1,8 @@
 ﻿using Dapper;
 using OnMed.Application.Utils;
 using OnMed.DataAccess.Interfaces.Heads;
-using OnMed.DataAccess.ViewModels;
 using OnMed.DataAccess.ViewModels.Heads;
 using OnMed.Domain.Entities.Heads;
-using System.Data.Common;
 
 namespace OnMed.DataAccess.Repositories.Heads;
 
--- src/OnMed.DataAccess/ViewModels/Administrators/AdministratorViewModel.cs
+++ src/OnMed.DataAccess/ViewModels/Administrators/AdministratorViewModel.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace OnMed.DataAccess.ViewModels.Administrators;
 
-namespace OnMed.DataAccess.ViewModels.Administrators
+public class AdministratorViewModel : HumanViewModel
 {
-    internal class AdministratorViewModel
-    {
-    }
 }
