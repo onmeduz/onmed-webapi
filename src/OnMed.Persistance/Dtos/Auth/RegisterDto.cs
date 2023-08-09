67539a0e070fc04f54993dbc2b7dd2a3dced6808
ahadulla <ahadulla1503@gmail.com>
2023-08-09 21:11:18 +0500

    Create Auth File

---
+++ src/OnMed.Persistance/Dtos/Auth/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnMed.Persistance.Dtos.Auth;
+
+public class RegisterDto
+{
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string PhoneNumber { get; set; } = String.Empty;
+    public string Password { get; set; } = string.Empty;
+}
