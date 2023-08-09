a53b89f4bba79ddfb93bf9bb43b79cc8c69244b2
ahadulla <ahadulla1503@gmail.com>
2023-08-09 15:04:19 +0500

    Create UserRepository

--- src/OnMed.DataAccess/Repositories/Users/UserRepository.cs
+++ src/OnMed.DataAccess/Repositories/Users/UserRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Dapper;
-using OnMed.Application.Utils;
 using OnMed.DataAccess.Interfaces.Users;
-using OnMed.DataAccess.ViewModels.Users;
-using OnMed.Domain.Entities.Administrators;
 using OnMed.Domain.Entities.Users;
 
 namespace OnMed.DataAccess.Repositories.Users;
@@ -14,7 +11,7 @@ public class UserRepository : BaseRepository, IUserRepository
         try
         {
             await _connection.OpenAsync();
-            string query = $"SELECT count(*) from users ;";
+            string query = $"SELECT count(*) FROM users ;";
             var result = await _connection.QuerySingleAsync<long>(query);
 
             return result;
@@ -34,7 +31,7 @@ public class UserRepository : BaseRepository, IUserRepository
         try
         {
             await _connection.OpenAsync();
-            string query = "INSERT INTO users(first_name, last_name, middle_name, birth_day, " +
+            string query = "INSERT INTO users (first_name, last_name, middle_name, birth_day, " +
                 "phone_number, phone_number_confirmed, is_male, image_path, region, password_hash, " +
                     "salt, created_at, updated_at) " +
                         "VALUES (@FirstName, @LastName, @MiddleName, @BirthDay, @PhoneNumber, @PhoneNumberConfirmed, " +
@@ -58,7 +55,7 @@ public class UserRepository : BaseRepository, IUserRepository
         try
         {
             await _connection.OpenAsync();
-            string query = $"select count(*) from users";
+            string query = $"DELETE FROM users WHERE id=@Id";
             var result = await _connection.QuerySingleAsync<int>(query);
 
             return result;
@@ -73,17 +70,12 @@ public class UserRepository : BaseRepository, IUserRepository
         }
     }
 
-    public Task<IList<UserViewModel>> GetAllAsync(PaginationParams @params)
-    {
-        throw new NotImplementedException();
-    }
-
     public async Task<User?> GetByIdAsync(long id)
     {
         try
         {
             await _connection.OpenAsync();
-            string query = $"SELECT * FROM users where id=@Id";
+            string query = $"SELECT * FROM users WHERE id=@Id";
             var result = await _connection.QuerySingleAsync<User>(query, new { Id = id });
 
             return result;
@@ -98,26 +90,15 @@ public class UserRepository : BaseRepository, IUserRepository
         }
     }
 
-    public Task<UserViewModel?> GetByIdViewModelAsync(long id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<(int ItemsCount, IList<UserViewModel>)> SearchAsync(string search, PaginationParams @params)
-    {
-        throw new NotImplementedException();
-    }
-
     public async Task<int> UpdateAsync(long id, User entity)
     {
         try
         {
             await _connection.OpenAsync();
-
             string query = "UPDATE users SET first_name=@FirstName, last_name=@lastName, middle_name=@MiddleName, " +
                     "birth_day=@BirthDay, phone_number=@PhoneNumber, phone_number_confirmed=@PhoneNumberConfirmed, " +
-                        "is_male=@IsMale, image_path=@ImagePath, region=@Region, password_hash=@PasswordHash, salt=@Salt, " +
-                            "created_at=CreatedAt, updated_at=@UpdatedAt " +
+                        "is_male=@IsMale, image_path=@ImagePath, region=@Region, password_hash=@PasswordHash, " +
+                            "salt=@Salt, updated_at=@UpdatedAt " +
                                 $"WHERE id = {id};";
 
             return await _connection.ExecuteAsync(query, entity);
