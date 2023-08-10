eaa26f3cd97bff9469e6781777a8122364921bcd
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-08-10 10:48:51 +0500

    added

--- src/OnMed.Persistance/Validators/Dtos/Categories/CategoryCreateValidator.cs
+++ src/OnMed.Persistance/Validators/Dtos/Categories/CategoryCreateValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using OnMed.Persistance.Common.Helpers;
 using OnMed.Persistance.Dtos.Categories;
 
 namespace OnMed.Persistance.Validators.Dtos.Categories;
@@ -13,14 +14,14 @@ public class CategoryCreateValidator : AbstractValidator<CategoryCreateDto>
         RuleFor(dto => dto.Professional).NotNull().NotEmpty().WithMessage("Professional field is required!")
             .MinimumLength(3).WithMessage("Professional must be more than 3 characters")
             .MaximumLength(50).WithMessage("Professional must be less than 50 characters");
-/*
-        int maxImageSizeMB = 3;
+
+        int maxImageSizeMB = 5;
         RuleFor(dto => dto.Image).NotEmpty().NotNull().WithMessage("Image field is required");
         RuleFor(dto => dto.Image.Length).LessThan(maxImageSizeMB * 1024 * 1024 + 1).WithMessage($"Image size must be less than {maxImageSizeMB} MB");
         RuleFor(dto => dto.Image.FileName).Must(predicate =>
         {
             FileInfo fileInfo = new FileInfo(predicate);
             return MediaHelper.GetImageExtensions().Contains(fileInfo.Extension);
-        }).WithMessage("This file type is not image file");*/
+        }).WithMessage("This file type is not image file");
     }
 }
--- src/OnMed.Persistance/Validators/Dtos/Categories/CategoryUpdateValidator.cs
+++ src/OnMed.Persistance/Validators/Dtos/Categories/CategoryUpdateValidator.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnMed.Service.Services
+namespace OnMed.Persistance.Validators.Dtos.Categories
 {
-    internal class Class1
+    internal class CategoryUpdateValidator
     {
     }
 }
