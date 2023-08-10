684db24abe9dee53aaf3c4855652a9bffcbed242
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-08-10 10:43:27 +0500

    Added

---
+++ src/OnMed.Persistance/Dtos/Categories/CategoryUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OnMed.Persistance.Dtos.Categories;
+
+public class CategoryUpdateDto
+{
+    public IFormFile? Image { get; set; }
+    public string Professionality { get; set; } = string.Empty;
+    public string ProfessionalityHint { get; set; } = string.Empty;
+    public string Professional { get; set; } = string.Empty;
+    public string ProfessionalHint { get; set; } = string.Empty;
+}
