3a68d7d03cbd6982ffd5819084aa33cbb13a44f6
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-08-21 15:16:04 +0500

    Remove image upload in HeadAdministratorController

--- src/OnMed.Persistance/Dtos/Administrators/AdministratorUpdateDto.cs
+++ src/OnMed.Persistance/Dtos/Administrators/AdministratorUpdateDto.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace OnMed.Persistance.Dtos.Administrators;
+﻿namespace OnMed.Persistance.Dtos.Administrators;
 
 public class AdministratorUpdateDto
 {
     public string FirstName { get; set; } = String.Empty;
     public string LastName { get; set; } = String.Empty;
     public string MiddleName { get; set; } = String.Empty;
-    public IFormFile? Image { get; set; }
     public string Region { get; set; } = String.Empty;
 }
