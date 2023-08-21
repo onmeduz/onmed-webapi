b1d7ea9365104574318e788fe5d92840eba9f770
ahadulla <ahadulla1503@gmail.com>
2023-08-21 08:35:10 +0500

    added

---
+++ src/OnMed.Persistance/Validators/Dtos/Administrators/AdministratorUpdateValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using OnMed.Persistance.Dtos.Administrators;
+
+namespace OnMed.Persistance.Validators.Dtos.Administrators;
+
+public class AdministratorUpdateValidator : AbstractValidator<AdministratorUpdateDto>
+{
+    public AdministratorUpdateValidator()
+    {
+        
+    }
+}
