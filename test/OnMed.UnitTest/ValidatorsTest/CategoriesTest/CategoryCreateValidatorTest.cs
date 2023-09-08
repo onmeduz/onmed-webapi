a0e978c3aefd81e7eaffa8c1f10cb089252e9d4a
ahadulla <ahadulla1503@gmail.com>
2023-09-08 18:42:31 +0500

    Create category validator test

---
+++ test/OnMed.UnitTest/ValidatorsTest/CategoriesTest/CategoryCreateValidatorTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using OnMed.Persistance.Dtos.Categories;
+using OnMed.Persistance.Validators.Dtos.Categories;
+using System.Text;
+using Xunit;
+
+namespace OnMed.UnitTest.ValidatorsTest.CategoriesTest;
+
+public class CategoryCreateValidatorTest
+{
+    [Theory]
+    [InlineData("djfkdkdlkdd","sdjkfkdlodd","djdkdkdd","ajsjdjd")]
+    [InlineData("dkdkckdkskd","sdjkfkdlodd","djdkdkdd","ajsjdjd")]
+    [InlineData("djfkdkdlkdd","sdjkfkdlodd","slkdlfkdklfk","ajsjdjd")]
+    [InlineData("mdkdkdkdkldkdkd","sdjkfkdlodd","skkdksmc","ajsjdjd")]
+    [InlineData("smskskskss","s,xdskslsls","sjdjjdjsajs","ajsjdjd")]
+
+
+    public void ValidCategoryCreateDto_ReturnsNoValidationErrors(
+        string professionality, string professionalityHint, string professional, string professionalHint)
+    {
+        byte[] byteImage = Encoding.UTF8.GetBytes("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+        IFormFile imageFile = new FormFile(new MemoryStream(byteImage), 0, byteImage.Length, "data", "file.jpg");
+
+        var dto = new CategoryCreateDto
+        {
+            Image = imageFile,
+            Professionality = professionality,
+            ProfessionalityHint = professionalityHint,
+            Professional = professional,
+            ProfessionalHint = professionalHint
+        };
+
+        var validationRules = new CategoryCreateValidator();
+        var result = validationRules.Validate(dto);
+
+        Assert.True(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData("", "sdjkfkdlodd", "djdkdkdd", "ajsjdjd")]
+    [InlineData("dkdkckdkskd", "sdjkfkdlodd", "", "ajsjdjd")]
+    [InlineData("", "sdjkfkdlodd", "slkdlfkdklfk", "ajsjdjd")]
+    [InlineData("mdkdkdkdkldkdkd", "sdjkfkdlodd", "", "ajsjdjd")]
+    [InlineData("", "sdjkfkdlodd", "", "ajsjdjd")]
+
+
+    public void InvalidCategoryCreateDto_ReturnsNoValidationErrors(
+        string professionality, string professionalityHint, string professional, string professionalHint)
+    {
+        byte[] byteImage = Encoding.UTF8.GetBytes("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+        IFormFile imageFile = new FormFile(new MemoryStream(byteImage), 0, byteImage.Length, "data", "file.jpg");
+
+        var dto = new CategoryCreateDto
+        {
+            Image = imageFile,
+            Professionality = professionality,
+            ProfessionalityHint = professionalityHint,
+            Professional = professional,
+            ProfessionalHint = professionalHint
+        };
+
+        var validationRules = new CategoryCreateValidator();
+        var result = validationRules.Validate(dto);
+
+        Assert.False(result.IsValid);
+    }
+}
