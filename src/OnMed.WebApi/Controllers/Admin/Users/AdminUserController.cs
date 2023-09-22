52a834c84ba988242b600672f311de284017d3f2
Muhammadali19969 <muhammadalirustamkulov160@gmail.com>
2023-09-22 19:37:59 +0500

    Move DataBase Connection

--- src/OnMed.WebApi/Controllers/Admin/Users/AdminUserController.cs
+++ src/OnMed.WebApi/Controllers/Admin/Users/AdminUserController.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnMed.Service.Interfaces.Hospitals;
 using OnMed.Service.Interfaces.Users;
 
 namespace OnMed.WebApi.Controllers.Admin.Users;
 
-[Route("api/admin/user")]
+[Route("api/admin/users")]
 [ApiController]
 public class AdminUserController : AdminBaseController
 {
     private readonly IUserAppointmentService _userAppointmentService;
+    private readonly IHospitalBranchService _hospitalBranchService;
 
-    public AdminUserController(IUserAppointmentService userAppointmentService)
+    public AdminUserController(IUserAppointmentService userAppointmentService,
+        IHospitalBranchService hospitalBranchService)
     {
         this._userAppointmentService = userAppointmentService;
+        this._hospitalBranchService = hospitalBranchService;
     }
 
     [HttpGet("patient/count/{hospitalBranchId}")]
     public async Task<IActionResult> CountByHospitalIdAsync(long hospitalBranchId)
     => Ok(await _userAppointmentService.CountByHospitalIdAsync(hospitalBranchId));
+
+    [HttpGet("patient/last-week/{hospitalBranchId}")]
+    public async Task<IActionResult> GetHospitalAppointmentFromLastWeek(long hospitalBranchId)
+    {
+        return Ok(await _hospitalBranchService.GetLastWeekAppointmentCount(hospitalBranchId));
+    }
 }
